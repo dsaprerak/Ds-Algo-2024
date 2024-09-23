b8c3664757539549e1e04d8e0d88bc4f725060b6
Anup-sharma <sharmaanup459@gmail.com>
2024-09-22 23:28:16 -0400

    updated challenge solution

--- mentees/anup-sharma/tests/challenge3.js
+++ mentees/anup-sharma/tests/challenge3.js
@@ -41,7 +41,39 @@ function createdisctictarray(arr){
     return disarr;
 }
 
+//Other way to do it 
 
+function oddoccuranceoutput(arr){
+
+    let output = [];
+    let map1 = new Map();
+    let i = 0;
+
+    
+        let count = 1;
+        for(let i of arr){
+            console.log(i);
+            if(map1.has(i)){
+                
+                map1.set(i, count++);
+            }
+            else{
+                
+                map1.set(i, count);
+            }
+            console.log(map1);
+       
+        }
+    
+    for(let [key,value] of map1){
+        if((value % 2) == 1){
+            output.push(key);
+        }
+    }
+
+    return output;
+}
 
 
 console.log(oddoccurrence([1,3,4,2,4,1,4]));
+console.log(oddoccuranceoutput([1,3,4,2,4,1,4]));
